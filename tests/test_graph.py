20808493c26d7cf5271fc2bc03ca96324afdd82b
Anj2307 <mc24bt005@iitdh.ac.in>
2026-07-22 20:09:54 +0530

    correct the graph test for test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,14 +1,25 @@
-from langchain_core.messages import HumanMessage
+from unittest.mock import MagicMock, patch
+
+from langchain_core.messages import AIMessage, HumanMessage
 
 from main import workflow
 
 
-def test_graph_runs():
+@patch("graph.nodes.llm")
+@patch("graph.nodes.llm_with_tools")
+def test_graph_runs(mock_llm_with_tools, mock_llm):
+
+    mock_llm_with_tools.invoke.return_value = AIMessage(
+        content="Python is a programming language."
+    )
+
+    mock_llm.invoke.return_value = MagicMock(content="Python Chat")
 
     config = {"configurable": {"thread_id": "pytest"}}
 
     result = workflow.invoke(
-        {"messages": [HumanMessage(content="What is Python?")]}, config=config
+        {"messages": [HumanMessage(content="What is Python?")]},
+        config=config,
     )
 
     assert "messages" in result
